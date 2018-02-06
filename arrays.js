c020344dc6496f346a16436ec0ab5eea8c9d0d0b
Rafael Huijon <rhuijonp@gmail.com>
2018-02-06 18:54:40 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,16 @@
+/*
+var ingridient1 = "bread";
+var ingridient2 = "mild cheese";
+var ingridient3 = "sharp cheese";
+var ingridient4 = "butter";
+var ingridient5 = "tomato";
+var ingridient6 = "garlic";
+*/
+
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(chocolateBars, element) {
+   var 
+   return newArray;
+}
+
