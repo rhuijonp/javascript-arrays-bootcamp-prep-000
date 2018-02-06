85df2a5e37634011bacf4fee746812f16f472e34
Rafael Huijon <rhuijonp@gmail.com>
2018-02-06 22:05:24 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -48,7 +48,9 @@ function destructivelyRemoveElementFromEndOfArray(array){
   return array;
 }
 
-function 
+function removeElementFromEndOfArray(array){
+  return array.slice(0,array.length - 1);
+}
 
 
 
