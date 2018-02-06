68647bc6e238af86fa00d3ef2cbe7ded1daad427
Rafael Huijon <rhuijonp@gmail.com>
2018-02-06 22:20:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -39,8 +39,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 } 
 
 function removeElementFromBeginningOfArray(array){
-  array.slice(1);
-  return array;
+  /*array.slice(1);*/
+  return array.slice(1);
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
