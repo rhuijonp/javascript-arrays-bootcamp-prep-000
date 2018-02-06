11805704a92f26fa7283aef17d7c2555cd852eed
Rafael Huijon <rhuijonp@gmail.com>
2018-02-06 20:29:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -12,7 +12,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(array, element) {
    
    var newArray = array;
-   array = [element,...array];
+   array = ['foo',1];
    return newArray;
 }
 
